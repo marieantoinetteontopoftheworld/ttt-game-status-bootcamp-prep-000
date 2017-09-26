c856134391ba0a049951e3e408af9f25fa413198
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-26 16:23:52 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,6 +27,7 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "0" && position_2 == "0" && position_3 == "0"
       combo = win_combination
+    end
   end
 
   if combo
