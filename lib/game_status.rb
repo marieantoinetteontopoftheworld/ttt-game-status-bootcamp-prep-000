9749bd460af8851a41d5a91f0ea6a96158356440
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-26 16:29:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,6 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combo|
     board[win_combo[0]] == board[won_combo[1]] &&
-    board[win_combo[1]] == board[won_combo[2]] &&
-    position_taken?(board, win_combo[0])
+    board[win_combo[1]] == board[won_combo[2]]
   end
 end
