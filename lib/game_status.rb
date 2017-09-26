f3cdf8e2e5ee2cac80f271727258432c65c5da9e
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-26 17:45:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,15 +15,16 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-
 def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
+  WIN_COMBINATIONS.detect do |win_combo|
+    board[win_combo[0]] == board[won_combo[1]] &&
+    board[win_combo[1]] == board[won_combo[2]] &&
+    position_taken?(board, win_combo[0])
   end
 end
 
 def full?(board)
-  board.all?{|token| token == "X" || token == "O"}
+  board.all? do |field|
+    field == "x" || filed == "0"
+  end
 end
