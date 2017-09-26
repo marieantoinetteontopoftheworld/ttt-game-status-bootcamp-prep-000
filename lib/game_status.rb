db388a78c45d281f55076e6e4e47f1051d3cf39f
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-26 16:02:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,20 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[index_1]
+    position_2 = board[index_2]
+    position_3 = board[index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "0" && position_2 == "0" && position_3 == "0"
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
+    end
+end
